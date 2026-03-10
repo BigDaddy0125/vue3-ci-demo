6f82e804a1b1c7080912bb71655e43ac4b3daf84
BigDaddy0125 <r466053280@gmail.com>
2026-03-10 15:52:41 +0900

    Switch to history routing with GitHub Pages SPA fallback

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 import DocsPage from './pages/docs/DocsPage.vue'
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/docs/:slug?', name: 'doc', component: DocsPage },
