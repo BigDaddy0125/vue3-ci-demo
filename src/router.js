c8aa7fa0e4de74891acde88bededa83012497653
BigDaddy0125 <r466053280@gmail.com>
2026-03-10 15:35:48 +0900

    Fix blank custom-domain page by removing hash history base

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Home from './pages/Home.vue'
 import DocsPage from './pages/docs/DocsPage.vue'
 
 export const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'home', component: Home },
     { path: '/docs/:slug?', name: 'doc', component: DocsPage },
